90d9c6709e24ff9b4d35459408bc8ae089028f7e
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-29 19:05:41 +0000

    Done.

--- nyc_api.rb
+++ nyc_api.rb
@@ -34,3 +34,7 @@ end
 
 programs = GetPrograms.new
 puts programs.program_school.uniq
+
+
+
+# we could easily adapt this code to be flexible and accept any URL we pass in using an initialize method and an instance variable. The get_programs method is really just getting the response body of from the requested URL, so we could name this get_response_body to be more accurate. We could replace the custom program_school method with a general parse_json method, as well. Instead of a specific class, we would instead have a class that retrieves JSON from any provided URL!
